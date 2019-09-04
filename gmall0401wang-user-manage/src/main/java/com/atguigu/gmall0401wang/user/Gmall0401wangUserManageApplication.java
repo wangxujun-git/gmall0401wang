0263aa1e105b5e8489dac6c1cc61526df99eab73
wangxujun <497922330@qq.com>
2019-09-04 18:54:48 +0800

    先加入

---
+++ gmall0401wang-user-manage/src/main/java/com/atguigu/gmall0401wang/user/Gmall0401wangUserManageApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall0401wang.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmall0401wangUserManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gmall0401wangUserManageApplication.class, args);
+	}
+
+}
